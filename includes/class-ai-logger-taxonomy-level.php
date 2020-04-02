5976ee14f642bcb28ced4dfb7f3259115f9e4bfc
Sean Fisher <srtfisher@gmail.com>
2020-04-01 21:22:35 -0400

    Cleaning up phpcs

--- includes/class-ai-logger-taxonomy-level.php
+++ includes/class-ai-logger-taxonomy-level.php
@@ -24,28 +24,32 @@ class AI_Logger_Taxonomy_Level extends AI_Logger_Taxonomy {
 	 * Create taxonomies.
 	 */
 	public function create_taxonomy() {
-		register_taxonomy( $this->name, $this->object_types, array(
-			'labels' => array(
-				'name'                  	=> __( 'Level', 'ai-logger' ),
-				'singular_name'         	=> __( 'Level', 'ai-logger' ),
-				'search_items'          	=> __( 'Search Levels', 'ai-logger' ),
-				'popular_items'         	=> __( 'Popular Levels', 'ai-logger' ),
-				'all_items'             	=> __( 'All Levels', 'ai-logger' ),
-				'edit_item'             	=> __( 'Edit Level', 'ai-logger' ),
-				'view_item'             	=> __( 'View Level', 'ai-logger' ),
-				'update_item'           	=> __( 'Update Level', 'ai-logger' ),
-				'add_new_item'          	=> __( 'Add New Level', 'ai-logger' ),
-				'new_item_name'         	=> __( 'New Level', 'ai-logger' ),
-				'add_or_remove_items'   	=> __( 'Add or remove Levels', 'ai-logger' ),
-				'choose_from_most_used' 	=> __( 'Choose from most used Levels', 'ai-logger' ),
-				'menu_name'             	=> __( 'Levels', 'ai-logger' ),
-				'not_found'             	=> __( 'No levels found', 'ai-logger' ),
-			),
-			'public' => false,
-			'show_admin_column' => true,
-			'show_in_nav_menus' => false,
-			'show_tagcloud' => false,
-		) );
+		register_taxonomy(
+			$this->name,
+			$this->object_types,
+			array(
+				'labels'            => array(
+					'name'                  => __( 'Level', 'ai-logger' ),
+					'singular_name'         => __( 'Level', 'ai-logger' ),
+					'search_items'          => __( 'Search Levels', 'ai-logger' ),
+					'popular_items'         => __( 'Popular Levels', 'ai-logger' ),
+					'all_items'             => __( 'All Levels', 'ai-logger' ),
+					'edit_item'             => __( 'Edit Level', 'ai-logger' ),
+					'view_item'             => __( 'View Level', 'ai-logger' ),
+					'update_item'           => __( 'Update Level', 'ai-logger' ),
+					'add_new_item'          => __( 'Add New Level', 'ai-logger' ),
+					'new_item_name'         => __( 'New Level', 'ai-logger' ),
+					'add_or_remove_items'   => __( 'Add or remove Levels', 'ai-logger' ),
+					'choose_from_most_used' => __( 'Choose from most used Levels', 'ai-logger' ),
+					'menu_name'             => __( 'Levels', 'ai-logger' ),
+					'not_found'             => __( 'No levels found', 'ai-logger' ),
+				),
+				'public'            => false,
+				'show_admin_column' => true,
+				'show_in_nav_menus' => false,
+				'show_tagcloud'     => false,
+			) 
+		);
 	}
 
 }
