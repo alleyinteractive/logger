a2d6c93c02a901d01a6b2d69075de8280ad794cb
srtfisher <srtfisher@users.noreply.github.com>
2022-04-08 17:27:03 +0000

    Built changes for v2.1.5-built

---
+++ vendor/alleyinteractive/wordpress-autoloader/CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog
+
+All notable changes to `alleyinteractive/wordpress-autoloader` will be
+documented in this file.
+
+## [Unreleased]
+
+## 0.1.1
+
+- Ensure autoloader root path always has a trailing slash.
+
+## 0.1.0
+
+- Initial release.
---
+++ vendor/composer/installed.php
@@ -0,0 +1,76 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'v2.1.5',
+    'version' => '2.1.5.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '30ac64ce3da6ce4cc15fb98eed47aac5260d9e5c',
+    'name' => 'alleyinteractive/logger',
+  ),
+  'versions' => 
+  array (
+    'alleyinteractive/composer-wordpress-autoloader' => 
+    array (
+      'pretty_version' => '0.2.0',
+      'version' => '0.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f01074ed8e31c0cc6f2841cd733450c4e2962b50',
+    ),
+    'alleyinteractive/logger' => 
+    array (
+      'pretty_version' => 'v2.1.5',
+      'version' => '2.1.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '30ac64ce3da6ce4cc15fb98eed47aac5260d9e5c',
+    ),
+    'alleyinteractive/wordpress-autoloader' => 
+    array (
+      'pretty_version' => 'v0.1.2',
+      'version' => '0.1.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a9ed691733b8bbc405bc2a0f86e5d382fc1058e8',
+    ),
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '2.5.0',
+      'version' => '2.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4192345e260f1d51b365536199744b987e160edc',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.4',
+      'version' => '1.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+    ),
+    'psr/log-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0.0 || 2.0.0 || 3.0.0',
+      ),
+    ),
+    'squizlabs/php_codesniffer' => 
+    array (
+      'pretty_version' => '3.6.2',
+      'version' => '3.6.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5e4e71592f69da17871dba6e80dd51bce74a351a',
+    ),
+  ),
+);
