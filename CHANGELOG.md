9e19597aeef5cf362898a1f131666206eefd6228
Sean Fisher <srtfisher@gmail.com>
2025-04-16 16:35:04 -0400

    Improve  to properly format the log message with colors

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,9 +3,10 @@
 This library adheres to [Semantic Versioning](https://semver.org/) and [Keep a
 CHANGELOG](https://keepachangelog.com/en/1.0.0/).
 
-## 2.6.0 - 2024-12-19
+## 2.6.0
 
 - Collapse internal calls to `do_action` and `apply_filters` in the log backtrace.
+- Improve `CLI_Handler` to properly format the log message with colors.
 
 ## 2.5.0 - 2024-09-03
 
--- inc/handler/class-cli-handler.php
+++ inc/handler/class-cli-handler.php
@@ -8,6 +8,7 @@
 namespace AI_Logger\Handler;
 
 use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Logger;
 
 /**
  * WP-CLI Handler to pipe logs to the wp-cli output.
@@ -21,13 +22,26 @@ class CLI_Handler extends AbstractProcessingHandler {
 	 * @param array $record Log Record.
 	 */
 	protected function write( array $record ): void {
-		[ 'formatted' => $formatted ] = $record;
-
-		// Ignore if the request isn't through WP-CLI.
 		if ( ! defined( 'WP_CLI' ) || ! WP_CLI ) {
 			return;
 		}
 
-		\WP_CLI::log( $formatted );
+		if ( empty( $record['level'] ) || empty( $record['message'] ) || empty( $record['level_name'] ) ) {
+			return;
+		}
+
+		$message = "Logger [{$record['level_name']}]: {$record['message']}";
+
+		if ( Logger::ERROR === $record['level'] ) {
+			$message = \WP_CLI::colorize( '%R' . $message . '%n' );
+		} elseif ( Logger::WARNING === $record['level'] ) {
+			$message = \WP_CLI::colorize( '%y' . $message . '%n' );
+		} elseif ( Logger::INFO === $record['level'] ) {
+			$message = \WP_CLI::colorize( '%g' . $message . '%n' );
+		} elseif ( Logger::DEBUG === $record['level'] ) {
+			$message = \WP_CLI::colorize( '%b' . $message . '%n' );
+		}
+
+		\WP_CLI::log( $message );
 	}
 }
